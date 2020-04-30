abcba98936e1a84e87c5f1aa6d5b24279db0caf3
rumoneandersonbiz <rumoneandersonbiz@gmail.com>
2020-04-30 12:45:01 -0500

    Revert "Merge remote-tracking branch 'origin/feature/game-polish' into bug/tmp-merge"
    This reverts commit bc74f06bc1f17d930f2fd70422da7ba6846106eb, reversing
    changes made to 8645ea5e94aabe104f6d5608f79f72ff11bef6ba.

--- Security Project/Assets/Scripts/SeverDialogue.cs
+++ Security Project/Assets/Scripts/SeverDialogue.cs
@@ -9,52 +9,18 @@ public class SeverDialogue : MonoBehaviour
 {
     public GameObject Panel;
     public GameObject Laptop;
-    public GameObject Panel2;
-    public GameObject Router;
-    public GameObject Text;
-    public void OnTriggerEnter2D(Collider2D other)
+     
+    public IEnumerator OnTriggerEnter2D(Collider2D other)
     {
        
         if(other.tag == "Player")
         {
             Panel.SetActive(true);
-        }
-
-        if(Laptop != null)
-        {
             Laptop.SetActive(true);
         }
 
-    }
-
-    public void GetInput(string answer)
-    {
-        
-        if(answer == "ilikestealing")
-        {
-            Panel.SetActive(false);
-            Text.GetComponent<Text>().text = "Correct";
-            Panel2.SetActive(true);
-            
-            Router.SetActive(true);
-            Debug.Log(Router);
-        }
-        else{
-            Debug.Log("Wrong!");
-        }
-    }
-
-    public void Close()
-    {
-        Panel2.SetActive(false);
-
-    }
-
-    public void CorrectButton()
-    {
+       yield return new WaitForSeconds(5);
         Panel.SetActive(false);
-        Panel2.SetActive(true);
     }
-
 
 }
