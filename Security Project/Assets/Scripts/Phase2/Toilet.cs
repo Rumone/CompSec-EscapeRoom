839e804fe31505d83209a5cbe3930bc95304e667
KingToast98 <32875984+ShannonH98@users.noreply.github.com>
2020-05-01 02:45:10 -0500

    Updated Couch

--- Security Project/Assets/Scripts/Phase2/Toilet.cs
+++ Security Project/Assets/Scripts/Phase2/Toilet.cs
@@ -2,20 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Couch : MonoBehaviour
+public class Toilet : MonoBehaviour
 {
     public bool canActivate;
     public GameObject cPanel;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(canActivate == true && Input.GetKeyUp("space"))
+        if (canActivate == true && Input.GetKeyUp("space"))
         {
             cPanel.SetActive(true);
             PlayerController.instance.canMove = false;
@@ -24,7 +24,7 @@ public class Couch : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if (collision.tag == "Player")
         {
             canActivate = true;
         }
